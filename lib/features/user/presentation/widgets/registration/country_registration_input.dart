9936a3a148f4a0ac26cf3f2b563817906300449b
Misa898 <misa@newmanstech.com>
2023-05-05 18:16:47 +0300

    add error validation in country

--- lib/features/user/presentation/widgets/registration/country_registration_input.dart
+++ lib/features/user/presentation/widgets/registration/country_registration_input.dart
@@ -5,6 +5,8 @@ import 'package:flutter_typeahead/flutter_typeahead.dart';
 import '../../../../../core/helper/countries_list.dart';
 import '../../bloc/registration_bloc/registration_bloc.dart';
 
+import 'package:gpt/core/models/required_input.dart';
+
 class CountryRegistrationInput extends StatefulWidget {
   const CountryRegistrationInput({super.key});
 
@@ -24,46 +26,53 @@ class _CountryRegistrationInputState extends State<CountryRegistrationInput> {
 
   @override
   Widget build(BuildContext context) {
-    return TypeAheadField(
-      hideSuggestionsOnKeyboardHide: true,
-      keepSuggestionsOnLoading: false,
-      hideOnEmpty: true,
-      debounceDuration: const Duration(milliseconds: 1000),
-      loadingBuilder: (context) => const SizedBox(
-          height: 100, child: Center(child: CircularProgressIndicator())),
-      textFieldConfiguration: TextFieldConfiguration(
-        controller: controller,
-        autofocus: false,
-        style: DefaultTextStyle.of(context)
-            .style
-            .copyWith(fontStyle: FontStyle.normal),
-        decoration: InputDecoration(
-          hintText: 'ex France...',
-          hintStyle:
-              TextStyle(fontSize: 12, color: Theme.of(context).hintColor),
-        ),
-      ),
-      suggestionsCallback: (pattern) async {
-        final suggest = countriesListJson
-            .where((element) => element['name']!.contains(pattern));
+    return BlocBuilder<RegistrationBloc, RegistrationState>(
+      builder: (context, state) {
+        return TypeAheadField(
+          hideSuggestionsOnKeyboardHide: true,
+          keepSuggestionsOnLoading: false,
+          hideOnEmpty: true,
+          debounceDuration: const Duration(milliseconds: 1000),
+          loadingBuilder: (context) => const SizedBox(
+              height: 100, child: Center(child: CircularProgressIndicator())),
+          textFieldConfiguration: TextFieldConfiguration(
+            controller: controller,
+            autofocus: false,
+            style: DefaultTextStyle.of(context)
+                .style
+                .copyWith(fontStyle: FontStyle.normal),
+            decoration: InputDecoration(
+              hintText: 'ex France...',
+              hintStyle:
+                  TextStyle(fontSize: 12, color: Theme.of(context).hintColor),
+              errorText: state.registrationInputs.country.isNotValid
+                  ? state.registrationInputs.country.displayError?.text
+                  : null,
+            ),
+          ),
+          suggestionsCallback: (pattern) async {
+            final suggest = countriesListJson
+                .where((element) => element['name']!.contains(pattern));
 
-        return suggest;
-      },
-      itemBuilder: (context, country) {
-        return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(country['name'] ?? ''),
+            return suggest;
+          },
+          itemBuilder: (context, country) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(country['name'] ?? ''),
+            );
+          },
+          onSuggestionSelected: (country) {
+            setState(() {
+              controller.text = country['name'] ?? '';
+            });
+            if (country['name'] != null) {
+              context
+                  .read<RegistrationBloc>()
+                  .add(RegistrationCountryChanged(country['name']!));
+            }
+          },
         );
-      },
-      onSuggestionSelected: (country) {
-        setState(() {
-          controller.text = country['name'] ?? '';
-        });
-        if (country['name'] != null) {
-          context
-              .read<RegistrationBloc>()
-              .add(RegistrationCountryChanged(country['name']!));
-        }
       },
     );
   }
